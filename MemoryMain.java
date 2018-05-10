08fcac561eb2fc7c784fdceca4b8882e50bd3511
brockdunsie <31520413+brockdunsie@users.noreply.github.com>
2018-05-10 13:47:30 -0600

    Initial

---
+++ MemoryMain.java
@@ -0,0 +1,10 @@
+public class MemoryMain {
+    public static void main(String[]args)
+    {
+        Card[][] grid = new Card[5][10];
+        for (Card[] g: grid
+             ) {
+
+        }
+    }
+}
