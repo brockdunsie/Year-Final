cf1df0f5b81a4dd854c380bb3626228c81007e84
brockdunsie <31520413+brockdunsie@users.noreply.github.com>
2018-05-10 14:19:34 -0600

    pissed

--- src/MemoryMain.java
+++ src/MemoryMain.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class MemoryMain {
     public static void main(String[]args)
     {
+        ArrayList<Card> deck = new ArrayList<>();
         Card[][] grid = new Card[5][10];
-        for (Card[] g: grid
-             ) {
+        for (Card g: deck
+
+                ) {
 
         }
     }
